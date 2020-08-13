12c2448590e1d4a969591125642de3d012ee273c
atchy <ashirakami@gmail.com>
2020-08-13 11:45:25 +0900

    feat: typescript対応とテストの追加

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -10,6 +10,14 @@ describe('My Login application', () => {
         expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+    it('should not login with invalid credentials', () => {
+        LoginPage.open();
+
+        LoginPage.login('tomsmith1', 'SuperSecretPassword!1');
+        expect(SecurePage.flashAlert).toBeExisting();
+        expect(SecurePage.flashAlert).toHaveTextContaining(
+            'Your username is invalid!');
+    });
 });
 
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -133,7 +133,7 @@ exports.config = {
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
         // TypeScript setup
-        requires: ['ts-node/register'],
+        requires: ['ts-node/register', 'tsconfig-paths/register'],
         // Jasmine default timeout
         defaultTimeoutInterval: 60000,
         //
